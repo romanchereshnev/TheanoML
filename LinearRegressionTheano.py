a8b3cddf0c660e36fbc60a0dcf9fb3b647c92125
roman.chereshnev <roman.chereshnev@gmail.com>
2017-08-21 02:26:48 +0300

    Loop in gradient descent was changed to Theano scan function

--- LinearRegressionTheano.py
+++ LinearRegressionTheano.py
@@ -20,7 +20,6 @@ class LinearRegressionTheano(object):
     """
     
     def __init__(self, seed=None):
-        
         self.__rng = np.random
         
         if not seed is None:
@@ -46,67 +45,6 @@ class LinearRegressionTheano(object):
             raise RuntimeError("{0} should have type {1}, not {2}".format(name, theano.config.floatX, X.dtype))
             
             
-    def __create_train_function(self, num_samples, num_features):
-        """Create and compile fitting function based on
-        gradient descient.
-        
-        Parameters
-        ----------
-        num_samples : {int}
-            Numbers of samples in train set.
-        num_features : {int}
-            Numbers of features in input data.
-            
-        Returns
-        -------
-        T : Theano function
-            Function takes as input:
-                X : {array}, shape=(num_saples, num_features)
-                    Input for regression.
-                y : {array} , shape=(num_saples, )
-                    Output for regression.
-                lamb: {float}
-                    Regression coefficient.
-                alpha: {float}
-                    Coefficient for gradient descient.
-            Function updates w and b vectors with respect
-            to gradient descient.
-            Function return:
-                cost : {float}
-                    Cost with respect to current w and b.
-                cost_with_reg : {float}
-                    Cost with respect to current w and b 
-                    with L2 regularization.
-        """   
-        # create random vectors as coefficients for regression
-        w = theano.shared(self.__rng.randn(num_features), name="w")
-        b = theano.shared(0., name="b")
-        
-        # create input variables
-        inp = T.dmatrix("inp")
-        out = T.dvector("out")
-        lamb = T.dscalar("lamb")
-        alpha = T.dscalar("alpha")
-       
-        prediction = T.dot(inp, w) + b # Linear fucntion
-        
-        cost = T.sum(T.pow(prediction - out, 2)) / (2*num_samples) # cost function
-        cost_with_reg = cost + lamb*T.mean(T.pow(w, 2)) # cost with regularization
-        gw, gb = T.grad(cost_with_reg, [w, b]) # calculate gradient 
-        
-        # compile Theano function
-        train = theano.function(inputs=[inp, out, lamb, alpha],
-                                outputs=[cost, cost_with_reg],
-                                updates=(
-                                         (w, w - alpha*gw),
-                                         (b, b - alpha*gb)
-                                        )
-                               )
-        
-        self.__w = w
-        self.__b = b
-        
-        return train
             
     def __create_predict_func(self):
         """ Create and compile linear function.
@@ -142,7 +80,18 @@ class LinearRegressionTheano(object):
                 Output of regression model.
         """   
         return self.predict_theano(X)
- 
+        
+    
+    def get_coefficients(self):
+        """Get coefficients of linear regression model
+                    
+        Returns
+            w : {array}, shape=(num_features, )
+                Coefficients of linear model.
+            b : {float}, shape=(num_saples, )
+                Bias of linear model.
+        """  
+        return self.__w, float(self.__b)
     
     def fit(self, X, y, reg_coef=0.0, a=0.001, max_iter=1000):
         """Fit model to create linear regression model.
@@ -169,27 +118,72 @@ class LinearRegressionTheano(object):
         self.__check(X, "X")
         self.__check(y, "y")
         
-        train = self.__create_train_function(X.shape[0], X.shape[1])
+        num_features = X.shape[1]
         
-        errs = []
-        errs_reg = []
-        for _ in range(max_iter):
-            err, err_reg = train(X, y, reg_coef, a)
-            errs.append(err)
-            errs_reg.append(err_reg)
+        def fn(cost, cost_with_reg, w, b, train_x, train_y, a, l):
+            """Itration function for Theano scan function. Function implement 
+            iteration of gradient descent
+            Parameters
+            ----------
+            costs : {float} 
+                Cost on previous gradient descient iteration.
+            costs_with_regression : {float} 
+                Cost with regresssion on previous gradient descient.
+            w : {array}, shape=(num_features)
+                Vector of parameters for linear regression
+            b : {float} 
+                Bias of linear regression
+            train_x : {array}, shape=(num_saples, num_features)
+                Input for fitting.
+            train_y : {array} , shape=(num_saples, )
+                Targets for fittings.
+            a: {float}
+                Coefficient for gradient descient.
+            l: {float}
+                Regression coefficient.
+            Return
+            -------
+            costs : {float}
+                Cost on current gradient descient iteration.
+            costs_with_regression : {float} 
+                Cost with regresssion on current gradient descient.
+            w : {array}, shape=(num_features)
+                Updated vector of parameters for linear regression
+            b : {float} 
+                Updated bias of linear regression
+            """ 
+            prediction = T.dot(train_x, w) + b # Linear fucntion
+
+            cost = T.mean( (prediction - train_y)**2 ) * 2 # cost function
+            cost_with_reg = cost + l*T.mean(w**2) # cost with regularization
+
+            gw, gb = T.grad(cost_with_reg, [w, b]) # calculate gradient 
+
+            w = w - a*gw # update w with gradint
+            b = b - a*gb # update b with gradint
+
+            return cost, cost_with_reg, w, b
+        
+        # create imput values for Theano
+        data_x = T.dmatrix()
+        data_y, w = T.dvectors("data_y", "w")
+        b, cost, cost_with_reg, alpha, lambd = \
+            T.dscalars("b", "cost", "cost_with_reg", "alpha", "lambd")
+        
+        # Loop function for gradient descent
+        result, updates = theano.scan(fn=fn, n_steps=max_iter, 
+                                      outputs_info=[cost, cost_with_reg, w, b],   
+                                      non_sequences=[data_x, data_y, alpha, lambd] 
+                                     )
+
+        # Output of loop 
+        costs, cost_with_regs, ws, bs = result
+        f = theano.function(inputs=[cost, cost_with_reg,  w, b, data_x, data_y, alpha, lambd], 
+                            outputs=[costs, cost_with_regs, ws[-1], bs[-1]], 
+                            updates=updates)
+        
+
+        self.costs, self.costs_with_regression, self.__w, self.__b = \
+            f(0.0, 0.0, self.__rng.randn(num_features), 0.0, X, y, a, reg_coef)
         
         self.__create_predict_func()
-        self.costs = np.array(errs)
-        self.costs_with_regression = np.array(errs_reg)
-        
-    
-    def get_coefficients(self):
-        """Get coefficients of linear regression model
-                    
-        Returns
-            w : {array}, shape=(num_features, )
-                Coefficients of linear model.
-            b : {float}, shape=(num_saples, )
-                Bias of linear model.
-        """  
-        return self.__w.get_value(), float(self.__b.get_value())
